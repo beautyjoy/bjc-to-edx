aea46dbf626b9fc175508b3643e65768a8999d0f
Peter Sujan <peterasujan@gmail.com>
2015-07-27 23:51:19 -0700

    Added a few files, got survey grading mostly working. Possibly will disable the check button and/or autosubmit after survey completion. Also, need to look into resizing of window after completion message appears.

---
+++ edx-js-problem/qualtrics.js
@@ -0,0 +1,13 @@
+
+console.log("TESTING TESTING");
+
+function receiveMessage() {
+    //console.log("qualtrics received message");
+    var complete = (document.getElementById("EndOfSurvey") !== null);
+    //console.log("trying to post message");
+    event.source.postMessage({"complete": complete}, "*");
+}
+
+window.addEventListener("message", receiveMessage, false);
+
+//window.onclick = receiveMessage;
