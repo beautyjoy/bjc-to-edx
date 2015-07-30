0a2feebc604dd490e06bf8b51dda6b353b716324
Michael Ball <cycomachead@gmail.com>
2015-07-30 00:59:00 -0700

    Progress on splitting 1 curriculum page to multiple sections

--- test.js
+++ test.js
@@ -15,7 +15,8 @@ curFolder = 'curriculum/edc/'
 output = './tmp/U2/';
 topic1 = 'nyc_bjc/1-intro-loops.topic';
 topic2 = 'nyc_bjc/2-conditionals-abstraction.topic';
-BASEURL = '/bjc-r';
+
+BASEURL = '/bjc-r'; // MATCH LLAB.ROOTURL IN CURR REPO
  topic = fs.readFileSync(util.topicPath(curFolder, topic1));
 //topic = fs.readFileSync(util.topicPath(curFolder, topic2));
 
@@ -28,13 +29,18 @@ CSSFILES = [
     'curriculum/edc/css/bjc.css'
 ]
 
-var combinedCSS = '<style>\n'
+var combinedCSS = CSSFILES.map(function(file) {
+    fs.readFileSync(file).toString();
+}).join('\n\n/******/\n\n');
 
-CSSFILES.forEach(function(file) {
-    combinedCSS += fs.readFileSync(file).toString();
-})
+fs.writeFileSync('./tmp/bjc-edx.css', combinedCSS);
 
-combinedCSS += '\n</style>\n\n'
+cssPath = util.edXPath('bjc-edx.css');
+cssString = '<link rel="stylesheet" href="' + cssPath + '">';
+
+function loadFile (path) {
+
+}
 
 data.topics.forEach(parseTopic);
 
@@ -46,7 +52,7 @@ function parseTopic (topic) {
 function parseSection (section) {
     if (section.title.indexOf(' Programming Lab') == 0 ||
         section.title.indexOf(' Investigation') == 0) {
-              
+
         dir = output + section.title;
         // Make if it doesn't exist.
         mkdirp.sync(dir);
@@ -57,13 +63,13 @@ function parseSection (section) {
             } else if (item.url.indexOf(BASEURL) != 0) {
                 return;
             }
-    
+
             count += 1;
             file = item.url.replace(BASEURL, curFolder);
             relPath = path.relative(curFolder, file);
             console.log('FILE: ', file);
             html = fs.readFileSync(file);
-            
+
             $ = cheerio.load(html);
             // Fix image URLs
             imgs = $('img');
@@ -71,23 +77,43 @@ function parseSection (section) {
                 url = imgs[i].attribs.src;
                 imgs[i].attribs.src = util.transformURL(BASEURL, relPath, url);
             }
-            
+
             // Fix Snap! run links.
             runs = $('a.run');
             for (var i = 0; i < runs.length; i += 1) {
                 url = runs[i].attribs.href;
                 runs[i].attribs.href = util.transformURL(BASEURL, relPath, url);
             }
-            
+
+            parts = [];
+            text = $('body').html()
             // parse quizes separately.
             quizzes = $('div.assessment-data');
             console.log('Found ', quizzes.length, ' quizzes.');
-            
-            data = combinedCSS + $('body').html();
-            fs.writeFileSync(dir + '/' + count + '-curriculum.html', data);
+            quizzes.each(function(index, elm) {
+                qzHTML = $.html(elm); // like a call to outerHTML()
+                var idx = text.indexOf(qzHTML);
+                parts.push(text.slice(0, idx)); // part before quiz
+                parts.push(qzHTML); // push quiz
+                text = text.slice(idx + qzHTML.length);
+            });
+
+            if (parts.length > 0) {
+                parts.forEach(function (item, idx, array) {
+                    data = cssString + text;
+                    fs.writeFileSync(dir + '/' + count + '-' + idx +
+                                        '-curriculum.html', data);
+                });
+            } else {
+                // FIXME.
+                data = cssString + text;
+                fs.writeFileSync(dir + '/' + count + '-curriculum.html', data);
+            }
         })
     }
 }
 
+// require('repl').start('> ');
 
-require('repl').start('> ');
+
+// QUIZ_REGEX = /(?=<div[.*?\n]class=["']assessment-data["'][.*?\n]>[.*?\n]</div>)/ig
