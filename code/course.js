c15347d5aa56168b498b99bffc7d1f608f71fb0a
Michael Ball <cycomachead@gmail.com>
2015-08-03 03:11:24 -0700

    wrote some more stuff to try to generalize....

--- code/course.js
+++ code/course.js
@@ -1,8 +1,11 @@
 
 var fs = require('fs');
+var child_process = require('child_process');
+
 var yaml = require('js-yaml');
 var et = require('elementtree');
-var child_process = require('child_process');
+// DOES THE PROCESSING AND SUCH
+var TEST_TEMP = require('../test.js');
 
 var course = {};
 var config, outline, outputDir;
@@ -10,6 +13,7 @@ var config, outline, outputDir;
 /** Copies the template xml course and fills in course/outline.xml with
  * pointers to chapter files. */
 var buildCourse = function(courseDirectory, options) {
+    // TODO: Check overrides in options. Store the defaults in some variable.
     config = yaml.load(fs.readFileSync(courseDirectory + 'config.yml'));
     outline = yaml.load(fs.readFileSync(courseDirectory + 'outline.yml'));
 
@@ -22,9 +26,9 @@ var buildCourse = function(courseDirectory, options) {
     var outlineXml = et.parse(fs.readFileSync(outlineFile).toString());
 
     outline.chapters.forEach(function(chapter) {
-    var chapterLoc = buildChapter(chapter);
-    var chapterXml = et.SubElement(outlineXml.getroot(), 'chapter'); // make new xml 'chapter' node, and append to course outline
-    chapterXml.set('url_name', chapterLoc); // set node's 'url_name' attribute to chapterLoc
+        var chapterLoc = buildChapter(chapter);
+        var chapterXml = et.SubElement(outlineXml.getroot(), 'chapter'); // make new xml 'chapter' node, and append to course outline
+        chapterXml.set('url_name', chapterLoc); // set node's 'url_name' attribute to chapterLoc
     });
     fs.writeFileSync(outlineFile, outlineXml.write({'xml_declaration': false})); // write back out to outlineFile
 };
@@ -33,7 +37,7 @@ course.buildCourse = buildCourse;
 /** Creates and fills in files in chapter/, each of which points to
  * several sequential files. Returns the filename of the created
  * chapter. */
-var buildChapter = function(chapterOutline) {
+var buildChapter = function (chapterOutline) {
     var chapterXml = new et.ElementTree(et.Element('chapter'));
     var chapterTitle = chapterOutline.title;
     chapterXml.getroot().set('display_name', chapterTitle);
@@ -50,16 +54,15 @@ var buildChapter = function(chapterOutline) {
 
 /** Creates and fills in files in sequential/, each of which points to
  * several vertical files. */
-var buildSequential = function(sequentialOutline) { // TODO: this function is very similar to buildChapter -- can probably generalize
+var buildSequential = function (sequentialOutline) { // TODO: this function is very similar to buildChapter -- can probably generalize
     var sequentialXml = new et.ElementTree(et.Element('sequential'));
     var sequentialTitle = sequentialOutline.title;
     sequentialXml.getroot().set('display_name', sequentialTitle);
     sequentialOutline.content.forEach(function(item) {
-    var verticalLocations = buildVerticals([item]);
-    verticalLocations.forEach(function(verticalLocation) {
-        var verticalNode = et.SubElement(sequentialXml.getroot(), 'vertical');
-        verticalNode.set('url_name', verticalLocation);
-    });
+        buildVerticals([item]).forEach(function(verticalLocation) {
+            var verticalNode = et.SubElement(sequentialXml.getroot(), 'vertical');
+            verticalNode.set('url_name', verticalLocation);
+        });
     });
     var fileName = sequentialTitle + '.xml';
     fs.writeFileSync(outputDir + 'sequential/' + fileName,
@@ -72,45 +75,48 @@ var buildSequential = function(sequentialOutline) { // TODO: this function is ve
  * etc). */
 var buildVerticals = function(verticalOutlines) {
     verticalOutlines.forEach(function(verticalOutline) {
-    var verticalXml, verticalTitle, itemXml;
-    verticalXml = new et.ElementTree(et.Element('vertical'));
-    verticalTitle = verticalOutline.title;
-    verticalXml.getroot().set('display_name', verticalTitle);
-    switch (verticalOutline.type) {
-    case 'file':
-        // TODO: this assumes that the path is already xml-ified, which
-        // will likely not be the case
-        itemXml = new et.SubElement(verticalXml, "html");
-        itemXml.set("url_name", verticalOutline.path);
-        return [verticalTitle];
-        break;
+        // TODO: Make this whole thing an external function
+        var verticalXml, verticalTitle, itemXml;
+        verticalXml = new et.ElementTree(et.Element('vertical'));
+        verticalTitle = verticalOutline.title;
+        verticalXml.getroot().set('display_name', verticalTitle);
+        // TODO: extract this to be a function
+        switch (verticalOutline.type) {
+        case 'file':
+            // TODO: this assumes that the path is already xml-ified, which
+            // will likely not be the case
+            itemXml = new et.SubElement(verticalXml, "html");
+            itemXml.set("url_name", verticalOutline.path);
+            return [verticalTitle];
+            break;
 
-    case 'quiz':
-        itemXml = new et.SubElement(verticalXml, "problem");
-        itemXml.set("url_name", verticalOutline.path);
-        return [verticalTitle];
-        break;
+        case 'quiz':
+            itemXml = new et.SubElement(verticalXml, "problem");
+            itemXml.set("url_name", verticalOutline.path);
+            return [verticalTitle];
+            break;
 
-    case 'video':
-        itemXml = new et.SubElement(verticalXml, "video");
-        itemXml.set("url_name", verticalOutline.path);
-        return [verticalTitle]; // TODO: this also needs to write a separate xml file in the 'video' directory
-        break;
+        case 'video':
+            itemXml = new et.SubElement(verticalXml, "video");
+            itemXml.set("url_name", verticalOutline.path);
+            return [verticalTitle]; // TODO: this also needs to write a separate xml file in the 'video' directory
+            break;
         
-    case 'external': // TODO: not sure exactly what this will be
-        break;
+        case 'external': // TODO: not sure exactly what this will be
+            break;
 
-    case 'llab':
-        var llabElements = test.michaelsFunction(verticalOutline.path, // TODO: rename once Michael has updated
-                             verticalOutline.section, 
-                             outputDir);
-        return llabElements.map(buildVerticals);
-        break;
+        case 'llab':
+            // TODO: rename once Michael has updated
+            var llabElements = TEST_TEMP(verticalOutline.path,
+                                 verticalOutline.section, 
+                                 outputDir);
+            return llabElements.map(buildVerticals);
+            break;
 
-    default:
-        throw 'Error: unrecognized vertical type "' + verticalOutline.type + '"';
-        break;
-    }
+        default:
+            throw 'Error: unrecognized vertical type "' + verticalOutline.type + '"';
+            break;
+        }
     });
 };
 
