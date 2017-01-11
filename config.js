b003291b05250cb0b506cb9794880c2fd46ecab7
Michael Ball <cycomachead@gmail.com>
2017-01-11 11:49:54 -0800

    more config planning

--- config.js
+++ config.js
@@ -1,8 +1,12 @@
-// A llab config file:
+// TODO: This isn't yet implemented.
+// A llab-builder config file:
+// This file will be `require`d by the main builder.
+// Given the nature of custom transforms this seems preferable
+// to a YAML file.
 module.exports = {
     baseURL: '/bjc-r',
-    contentFolder: 'curriculum/bjc-r',
-    edc_labs: {
+    contentFolder: 'curriculum/', // the directory containing the folder of "baseURL"
+    edc_labs: { // TODO: where should random custom data be put?
         1: 'intro-loops.topic',
         2: 'conditionals-abstraction.topic',
         3: 'lists.topic',
@@ -12,7 +16,7 @@ module.exports = {
         7: 'recursive-reporters.topic'
     },
     outputDirectory: './tmp/',
-    cssFiles: [
+    cssFiles: [ // inside the curriculum folder
         '/llab/css/3.3.0/bootstrap-compiled.min.css',
         '/llab/css/default.css',
         '/css/bjc.css'
@@ -20,5 +24,16 @@ module.exports = {
     compliedCSSName: 'bjc-edx.css',
     additionalScripts: [
         'edx-llab-hack.js'
+    ],
+    // These are functions which can use Cheerio to modify page html.
+    // They are executed in this order.
+    htmlTransforms: [
+        rewriteImageURLs,
+        rewriteSnapURLs,
+        transformLocalFiles,
     ]
 };
+
+function rewriteImageURLs() {}
+function rewriteSnapURLs() {}
+function transformLocalFiles() {}
